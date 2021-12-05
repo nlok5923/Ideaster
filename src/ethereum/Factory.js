089b2ae7525162f87ccef8529a4cbd800894c4fd
nlok5923 <nlokhande5923@gmail.com>
2021-12-05 21:35:27 +0530

    fixed ideaster

--- src/ethereum/Factory.js
+++ src/ethereum/Factory.js
@@ -3,7 +3,7 @@ import Manager from "./build/ideaFactory.json";
 
 const instance = new web3.eth.Contract(
   JSON.parse(Manager.interface),
-  "0x5749FA936de1Bf61AB23220256A6f295F673cAA6"
+  "0x978305105F21Cba46BD8B7Ab8C112E21e841F148"
 );
 
 export default instance;
